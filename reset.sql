f5512490cb75601cbe9b1740dcbc61f223d98f16
bryndisgunnlaugsd <bryndisg24@ru.is>
2026-09-24 15:24:17 +0000

    fyrsta migration, populate database og reset scripta

---
+++ reset.sql
@@ -0,0 +1,8 @@
+/* scripta til að reset-a gagnagrunninn */
+DELETE FROM AlbumArtist;
+DELETE FROM ArtistGenre;
+DELETE FROM Songs;
+DELETE FROM Albums;
+DELETE FROM Artists;
+DELETE FROM Genres;
+DELETE FROM sqlite_sequence;
